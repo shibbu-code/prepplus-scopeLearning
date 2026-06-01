5a954b69a0444fbe924427faacf2cd80cc0313f4
shibbu-code <shivambgi18@gmail.com>
2026-06-01 20:47:10 +0530

    anushka error

--- Backend/Auth/OtpSystem/emailfun.js
+++ Backend/Auth/OtpSystem/emailfun.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
    host: "smtp.gmail.com",
   port: 587,
   secure: false, // Use true for port 465, false for port 587
+  family: 4, // Use IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
