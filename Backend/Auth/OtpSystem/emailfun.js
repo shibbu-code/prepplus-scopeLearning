0489c89c593238358cabdcc9cea617d47d560541
shibbu-code <shivambgi18@gmail.com>
2026-06-01 19:56:35 +0530

    smtp fix

--- Backend/Auth/OtpSystem/emailfun.js
+++ Backend/Auth/OtpSystem/emailfun.js
@@ -2,14 +2,24 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 const transporter = nodemailer.createTransport({
    host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // Use true for port 465, false for port 587
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   },
 });
+
 const SendEmail = async (email, otp) => {
+transporter.verify((error, success) => {
+  if (error) {
+    console.error("SMTP Error:", error);
+  } else {
+    console.log("SMTP Ready");
+  }
+});
+
+
   const info = await transporter.sendMail({
     from: `"Prepplus Organization" <${process.env.EMAIL_USER}>`,
     to: email,
