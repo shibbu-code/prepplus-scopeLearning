127c1575465c0291f8bea057a64a1479ce02ea35
shibbu-code <shivambgi18@gmail.com>
2026-06-01 11:38:55 +0530

    db reach check

--- Backend/DB/dbconnection.js
+++ Backend/DB/dbconnection.js
@@ -4,6 +4,8 @@ const connectDB = async (Url) => {
     try {
         await mongoose.connect(Url);
         console.log('MongoDB connected successfully');
+        console.log("Connected DB:", mongoose.connection.name);
+        console.log("Connected Host:", mongoose.connection.host);
     } catch (err) {
         console.error('MongoDB connection error:', err);
         process.exit(1); // Exit process with failure
